91163b4c55e722f2ba1c084a046d41eb3db5510e
mfalling <mfalling@usf.edu>
2021-04-04 18:09:49 -0400

    Load data and script

---
+++ script.R
@@ -0,0 +1,140 @@
+# Library -----------------------------------------------------------------
+
+library(sf)
+library(dplyr)
+
+library(rgdal)
+library(raster)
+
+# About -------------------------------------------------------------------
+
+# Partially following https://www.youtube.com/watch?v=WBfcR0zN0xk
+# video entitled: "How to get the bounding box of spatial polygons in R"
+
+# Partially following datacamp.com/courses/spatial-analysis-with-sf-and-raster-in-r
+# DataCamp course entitled: "Spatial Analysis with sf and raster in R"
+
+# Sample Shape File from https://docs.mapbox.com/help/glossary/shapefile/
+
+
+# Exploring shape file ----------------------------------------------------
+
+
+# Reading shp file using sf package.
+stations <- st_read("stations/stations.shp")
+
+# Reads as a dataframe?
+str(stations)
+class(stations) # sf and data.frame class
+
+# View first six records: Spatial metadata
+head(stations)
+
+# Geometry column is class "sfc_POINT" and "sfc"
+class(stations$geometry)
+
+# Get number of stations per line
+stations %>%
+  group_by(line) %>%
+  tally()
+
+# Most stations are on the "blue" or "green" lines. 
+# dplyr tally output contains the metadata
+
+# Get area. What does this mean?
+st_area(stations)
+
+# Output produces vector of zeroes?
+str(st_area(stations))
+class(st_area(stations))
+
+# Isolate a point
+stations$geometry[1]
+
+st_area(stations[1])
+
+# Not sure why this is all zeroes.
+
+
+# Troubleshooting ---------------------------------------------------------
+
+# Following https://r-spatial.github.io/sf/reference/geos_measures.html
+
+geometries <- rbind(c(-1,-1), 
+                    c(1,-1), 
+                    c(1,1), 
+                    c(-1,1), 
+                    c(-1,-1)) # Why is this point listed twice?
+
+# Make a polygon
+b0 <- st_polygon(list(geometries))
+b0
+b1 <- b0 + 2
+b1
+b2 <- b0 + c(-0.2, 2)
+b2
+
+# "Create simple feature geometry list column, set class, and add coordinate reference system and precision"
+x <- st_sfc(b0, b1, b2)
+
+# Get area
+st_area(x)
+
+
+#   -----------------------------------------------------------------------
+
+# Not a multipolygon: Only two points.
+stations$geometry[1]
+
+# Combine two
+rbind(stations$geometry[1],
+      stations$geometry[2])
+
+# Not the desired result. Attempt to extract points
+st_coordinates(stations$geometry)
+
+# Combining two
+geometries2 <- rbind(st_coordinates(stations$geometry[1]),
+                    st_coordinates(stations$geometry[2]))
+b0_2 <- st_polygon(list(geometries2))
+
+# Error: "polygons not (all) closed"
+
+geometries2 <- rbind(st_coordinates(stations$geometry[1]),
+                     st_coordinates(stations$geometry[2]),
+                     st_coordinates(stations$geometry[1])) # Listing point twice to close polygon.
+b0_2 <- st_polygon(list(geometries2))
+
+# Transform polygon into a geometry set.
+x_2 <- st_sfc(b0_2)
+x_2
+
+# Get the area of this polygon.
+st_area(x_2)
+# Literally just a line. Area is zero. Expected.
+
+# Better example:
+# How much distance is between these four stations?
+stations$name[1:4]
+
+# Create a polygon using the four stations as points. 
+# Don't forget to close the polygon by adding the first point to the end of the coordinate list.
+station_poly <- st_polygon(list(rbind(st_coordinates(stations$geometry[1:4]), 
+                              st_coordinates(stations$geometry[1]))))
+
+# Transform polygon into a geometry set.
+station_polyset <- st_sfc(station_poly)
+
+# Get the area
+st_area(station_polyset)
+
+# Cool. I have no idea what this means.
+
+# View points
+station_poly
+
+# Plot the polygon
+plot(station_poly)
+
+# This is a visualization of the space between four stations. Not superimposed on a map.
+
